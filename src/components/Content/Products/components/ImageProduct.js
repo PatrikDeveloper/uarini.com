d530c52207c512d5aaacc9a00a7d27ca4b59db69
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-02-06 14:25:54 -0300

    Updating readme information

---
+++ src/components/Content/Products/components/ImageProduct.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 150px;
+  background: #ccc;
+`;
+
+export default function ImageProduct() {
+  return(
+    <Wrapper></Wrapper>
+  )
+}
--- src/components/Content/Products/components/SectionProduct.js
+++ src/components/Content/Products/components/SectionProduct.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import ImageProduct from './ImageProduct';
+import DescriptionProduct from './DescriptionProduct';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  width: 200px;
+  height: 300px;
+  border: 1px solid #000;
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
+
+
 
 export default function SectionProduct() {
   return (
-    <Wrapper>ssssss</Wrapper>    
+    <Wrapper>
+      <ImageProduct />
+      <DescriptionProduct />
+    </Wrapper>    
   );
 }
