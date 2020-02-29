c931e2502d150bf6b71ab757917892c5120042d1
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-02-28 23:01:59 -0300

    Created docs

---
+++ docs/precache-manifest.7916211e95a68a74f0490a9071ce507a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a33b23e7c45b66d0a3ad9fc3f31bf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21247a4e030ca079bf91",
+    "url": "/static/css/main.add5afc2.chunk.css"
+  },
+  {
+    "revision": "10be4206f2a71166aab4",
+    "url": "/static/js/2.a1ce6e77.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a1ce6e77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21247a4e030ca079bf91",
+    "url": "/static/js/main.56be802b.chunk.js"
+  },
+  {
+    "revision": "d9853cfeb4dff2ac7d34",
+    "url": "/static/js/runtime-main.aecdc067.js"
+  },
+  {
+    "revision": "38db253d22a34e2241b19352d400ee13",
+    "url": "/static/media/acai-amazonas.38db253d.jpg"
+  },
+  {
+    "revision": "7db6756a9f979246134cf298e3768ea9",
+    "url": "/static/media/banner1.7db6756a.jpg"
+  },
+  {
+    "revision": "96f44a2576de41f6b98f20d28f1b96b0",
+    "url": "/static/media/castanha-para.96f44a25.jpg"
+  },
+  {
+    "revision": "607d7ae7a61666bf166b9fa13ead5747",
+    "url": "/static/media/configuration.607d7ae7.svg"
+  },
+  {
+    "revision": "5c64836261a88873bf9b7e04d048d63a",
+    "url": "/static/media/delivery.5c648362.svg"
+  },
+  {
+    "revision": "93e076e96ce46a6ad292fc6acba216e0",
+    "url": "/static/media/farinha-branca.93e076e9.png"
+  },
+  {
+    "revision": "0e15963126abcb5e704cd6df72224c0f",
+    "url": "/static/media/home.0e159631.svg"
+  },
+  {
+    "revision": "2a1b31f9ed8a74f16e24baa8f9253d59",
+    "url": "/static/media/ovinha.2a1b31f9.jpg"
+  },
+  {
+    "revision": "524a30a365805e430446524968753f38",
+    "url": "/static/media/rede.524a30a3.png"
+  },
+  {
+    "revision": "37e981f166e8e4a6c169bdb546abaf57",
+    "url": "/static/media/tucuma.37e981f1.jpg"
+  }
+]);
