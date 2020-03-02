ea20ef419c74c6a7542509d8e01c7e82223873b9
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-03-02 17:03:04 -0300

    Adding one more form field

---
+++ src/view/Shopping/BuyProduct/Forms.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';  
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '100%',
+    },
+  },
+}));
+
+const Wrapper = styled.div`
+  padding: 20px 10px;
+`;
+
+const WrapperPrice = styled.div`
+  padding: 20px 0;
+`;
+
+const Price = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+  font-size: 38px;
+  color: #ff8c00;
+  font-weight: bold;
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  padding: 0 20px 0 10px ;
+`;
+
+const Amount = styled.button`
+  display: inline-block;
+  vertical-align: middle;
+  padding: 5px 25px;
+  margin-left: 2px; 
+  font-size: 22px;
+  font-weight: bold;
+  color: #24292e;
+  border: 2px solid #ccc;
+  background: #Fafafa;
+  border-radius: 5px;
+  cursor: pointer;
+  outline: none;
+
+  &:hover {
+    background: #dcdcdc;
+  }
+`;
+
+const ShowQuantity = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+  font-size: 22px;
+  font-weight: bold;
+  border: 2px solid #ccc;
+  border-radius: 15px;
+  margin-left: 10px;
+  padding: 5px 20px;
+  color: #ccc;
+`;
+
+
+export default function Forms() {
+  const classes = useStyles();
+
+  return (
+    <Wrapper>
+
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="outlined-basic" label="Nome Completo" variant="outlined" />
+        <TextField id="outlined-basic" label="Endereço" variant="outlined" />
+        <TextField id="outlined-basic" label="(11)12345-6789" variant="outlined" />
+      </form>
+
+      <WrapperPrice>
+        <Price>R$: 25,00</Price>
+        <Amount>+</Amount>
+        <Amount>-</Amount>
+        <ShowQuantity>1</ShowQuantity>
+      </WrapperPrice>
+    </Wrapper>
+  )
+}
